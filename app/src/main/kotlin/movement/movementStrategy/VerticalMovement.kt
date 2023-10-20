ca0a282f5fdd06bafdd380dc1f54adcd151a71a3
Franco B <franco.barravecchia2002@gmail.com>
2023-10-20 10:44:01 -0300

    feat: VerticalMovement

---
+++ app/src/main/kotlin/movement/movementStrategy/VerticalMovement.kt
@@ -0,0 +1,33 @@
+package movement.movementStrategy
+
+import board.Board
+import movement.MovementStrategy
+import pieceEatingRuler.PieceEatingRuler
+import player.Player
+import vector.Vector
+
+class VerticalMovement: MovementStrategy {
+    override fun checkMovement(pieceEatingRuler: PieceEatingRuler, player: Player, actual: Vector,
+                               destination: Vector, board: Board): Boolean {
+        if (!isVertical(actual, destination)) return false
+        return !checkIfPieceInMiddlePath(actual, destination, board)
+    }
+
+    private fun isVertical(origin: Vector, destini: Vector): Boolean{
+        if (origin.x != destini.x) return false
+        if (origin.y == destini.y) return false
+        return true
+    }
+
+    private fun checkIfPieceInMiddlePath(origin: Vector, destini: Vector, board: Board): Boolean{
+        if (origin.y < destini.y) return checkIfPieceBetweenYMinAndYMax(origin.y, destini.y, origin.x, board)
+        return checkIfPieceBetweenYMinAndYMax(destini.y, origin.y, origin.x, board)
+    }
+
+    private fun checkIfPieceBetweenYMinAndYMax(yMin: Int, yMax: Int, x: Int, board: Board): Boolean{
+        for (y in yMin+1 until yMax)
+            if (board.getPieceInPosition(Vector(x, y)).isSuccess)
+                return true
+        return false
+    }
+}
