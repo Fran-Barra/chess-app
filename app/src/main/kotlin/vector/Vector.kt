f49d24b9abc3fdeed07f4f75c3e432179279adaf
Franco B <franco.barravecchia2002@gmail.com>
2023-10-21 16:35:10 -0300

    update: vector operations

--- app/src/main/kotlin/vector/Vector.kt
+++ app/src/main/kotlin/vector/Vector.kt
@@ -5,6 +5,18 @@ class Vector (val x: Int, val y: Int){
         return Vector(x + other.x, y + other.y);
     }
 
+    operator fun minus(other: Vector): Vector {
+        return Vector(x - other.x, y - other.y)
+    }
+
+    operator fun times(other: Vector): Int{
+        return x*other.x + y*other.y
+    }
+
+    operator fun times(mul: Int): Vector{
+        return Vector(x*mul, y*mul)
+    }
+
     override fun equals(other: Any?): Boolean {
         if (other !is Vector) return false
         if (x != other.x) return false
