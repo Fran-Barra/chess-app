04589e7c1843fcdcdf96bafcfb0f8c21bac97dfe
Franco B <franco.barravecchia2002@gmail.com>
2023-10-21 16:04:30 -0300

    fix: math pow

--- app/src/main/kotlin/myMath/math.kt
+++ app/src/main/kotlin/myMath/math.kt
@@ -1,7 +1,8 @@
 package myMath
 
-public fun pow(base: Int, pow: Int): Int {
-    if (base == 0) return 0
-    if (base == 1) return 1
+fun pow(base: Int, pow: Int): Int {
+    if (base == 0 || base == 1) return base
+    if (pow == 0) return 1
+    if (pow == 1) return base
     return base * pow(base, pow-1)
 }
