d6864ace9ce0dd2e3cc8860b54b378ef505e6b0f
Franco B <franco.barravecchia2002@gmail.com>
2023-10-26 09:31:36 -0300

    feat: BasicChessMovement

---
+++ app/src/main/kotlin/movement/movementStrategy/gameMovementsFactory/BasicChessMovements.kt
@@ -0,0 +1,17 @@
+package movement.movementStrategy.gameMovementsFactory
+
+import movement.MovementStrategy
+import movement.movementStrategy.movementStrategyFactory.*
+
+object BasicChessMovements: GameMovementsFactory {
+    override fun getMovementsStrategies(): Map<Int, MovementStrategy> {
+        val movementStrategies: MutableMap<Int, MovementStrategy> = mutableMapOf()
+        movementStrategies[0] = KingMovementStrategy.getMovementStrategy()
+        movementStrategies[1] = QueenMovementStrategy.getMovementStrategy()
+        movementStrategies[2] = BishopMovementStrategy.getMovementStrategy()
+        movementStrategies[3] = KnightMovementStrategy.getMovementStrategy()
+        movementStrategies[4] = RookMovementStrategy.getMovementStrategy()
+        movementStrategies[5] = PawnMovementStrategy.getMovementStrategy()
+        return movementStrategies
+    }
+}
