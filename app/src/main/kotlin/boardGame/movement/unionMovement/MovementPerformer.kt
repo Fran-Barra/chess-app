e857719c08c8eeca817cfdd1ff6c436945b725f0
Franco B <franco.barravecchia2002@gmail.com>
2023-11-03 22:59:26 -0300

    feat: MovementPerformer

---
+++ app/src/main/kotlin/boardGame/movement/unionMovement/MovementPerformer.kt
@@ -0,0 +1,14 @@
+package boardGame.movement.unionMovement
+
+import boardGame.board.Board
+import boardGame.board.Vector
+import boardGame.piece.Piece
+
+interface MovementPerformer {
+    fun performMovement(piecePosition: Vector, too: Vector, board: Board): MovementResult
+}
+
+data class MovementResult(val newBoard: Board, val movementEvents: List<MovementEvent>)
+sealed interface MovementEvent
+data class Move(val piece: Piece, val from: Vector, val too: Vector): MovementEvent
+data class Eat(val piece: Piece, val position: Vector, val eater: Piece): MovementEvent
