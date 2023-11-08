7823d95e2f5c4cc061a4d193fef2f930cb5e98e1
Franco B <franco.barravecchia2002@gmail.com>
2023-11-07 22:22:09 -0300

    fix: princess movement

--- app/src/main/kotlin/chess/movementStrategy/movementStrategyFactory/PrincessMovementStrategy.kt
+++ app/src/main/kotlin/chess/movementStrategy/movementStrategyFactory/PrincessMovementStrategy.kt
@@ -2,12 +2,12 @@ package chess.movementStrategy.movementStrategyFactory
 
 import boardGame.movement.MovementValidator
 import boardGame.movement.MovementStrategyFactory
+import boardGame.movement.unionMovement.AndUnionMovementValidator
 import chess.movementStrategy.DistanceLimitMovement
-import boardGame.movement.unionMovement.OrUnionMovementValidator
 
 object PrincessMovementStrategy: MovementStrategyFactory {
     private val strategy: MovementValidator by lazy {
-        OrUnionMovementValidator(listOf(DistanceLimitMovement(5), QueenMovementStrategy.getMovementStrategy()))
+        AndUnionMovementValidator(listOf(DistanceLimitMovement(4), QueenMovementStrategy.getMovementStrategy()))
     }
     override fun getMovementStrategy(): MovementValidator {
         return strategy
