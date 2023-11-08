e42e5bffe33c2fb8ecb5191952716a302a06f0a5
Franco B <franco.barravecchia2002@gmail.com>
2023-11-08 14:34:10 -0300

    fix: to object

--- app/src/main/kotlin/chess/movementStrategy/gameMovementsFactory/BasicChessMovements.kt
+++ app/src/main/kotlin/chess/movementStrategy/gameMovementsFactory/BasicChessMovements.kt
@@ -9,18 +9,19 @@ object BasicChessMovements: GameMovementsFactory {
     override fun getMovementsManager(): MovementManager {
         val movementStrategies: MutableMap<Int, List<Movement>> = mutableMapOf()
         movementStrategies[0] = listOf(
-            Movement(KingMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()),
-            Movement(CastlingValidator(), CastlingPerformer())
+            Movement(KingMovementStrategy.getMovementStrategy(), FromTooMovementPerformer),
+            Movement(CastlingValidator, CastlingPerformer)
             )
-        movementStrategies[1] = listOf(Movement(QueenMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[2] = listOf(Movement(BishopMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[3] = listOf(Movement(KnightMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[4] = listOf(Movement(RookMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[5] = listOf(Movement(PawnMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
+        movementStrategies[1] = listOf(Movement(QueenMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[2] = listOf(Movement(BishopMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[3] = listOf(Movement(KnightMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[4] = listOf(Movement(RookMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[5] = listOf(Movement(PawnMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
         return PieceTypeMovementManager(movementStrategies)
     }
 
     override fun getMovementsManagerController(): MovementManagerController {
-        TODO("Not yet implemented")
+        //TODO: implement
+        return BaseMovementManagerController(listOf())
     }
 }
--- app/src/main/kotlin/chess/movementStrategy/gameMovementsFactory/RebellionChessMovements.kt
+++ app/src/main/kotlin/chess/movementStrategy/gameMovementsFactory/RebellionChessMovements.kt
@@ -6,16 +6,16 @@ import chess.movementStrategy.movementStrategyFactory.*
 object RebellionChessMovements: GameMovementsFactory {
     override fun getMovementsManager(): MovementManager {
         val movementStrategies: MutableMap<Int, List<Movement>> = mutableMapOf()
-        movementStrategies[0] = listOf(Movement(KingMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[1] = listOf(Movement(QueenMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[6] = listOf(Movement(PrincessMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[3] = listOf(Movement(KnightMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[4] = listOf(Movement(RookMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
-        movementStrategies[5] = listOf(Movement(PawnMovementStrategy.getMovementStrategy(), FromTooMovementPerformer()))
+        movementStrategies[0] = listOf(Movement(KingMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[1] = listOf(Movement(QueenMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[6] = listOf(Movement(PrincessMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[3] = listOf(Movement(KnightMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[4] = listOf(Movement(RookMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
+        movementStrategies[5] = listOf(Movement(PawnMovementStrategy.getMovementStrategy(), FromTooMovementPerformer))
         return PieceTypeMovementManager(movementStrategies)
     }
 
     override fun getMovementsManagerController(): MovementManagerController {
-        TODO("Not yet implemented")
+        return BaseMovementManagerController(listOf())
     }
 }
