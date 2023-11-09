585935fbeba80e23be8069d5b21c1b52b3a3a742
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 10:01:29 -0300

    feat: ValidateAdvancing checkers

---
+++ app/src/main/kotlin/checkers/movement/validator/ValidateAdvancing.kt
@@ -0,0 +1,16 @@
+package checkers.movement.validator
+
+import boardGame.board.Board
+import boardGame.board.Vector
+import boardGame.movement.MovementValidator
+import boardGame.pieceEatingRuler.PieceEatingRuler
+import boardGame.player.Player
+
+object ValidateAdvancing: MovementValidator {
+    override fun validate(pieceEatingRuler: PieceEatingRuler, player: Player, actual: Vector,
+                          destination: Vector, board: Board
+    ): Boolean {
+        if (player.getPlayerId() == 0) return destination.y < actual.y
+        return destination.y > actual.y
+    }
+}
