109c56c555fc545d74c0fc72baebc08e7f00e5df
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 09:56:32 -0300

    feat: NormalPieceMovement checkers

---
+++ app/src/main/kotlin/checkers/movement/validator/composedValidators/NormalPieceMovement.kt
@@ -0,0 +1,20 @@
+package checkers.movement.validator.composedValidators
+
+import boardGame.movement.MovementStrategyFactory
+import boardGame.movement.MovementValidator
+import boardGame.movement.unionMovement.AndUnionMovementValidator
+import checkers.movement.validator.EmptyDestination
+import checkers.movement.validator.NoPieceCanEat
+import chess.movementStrategy.validators.DiagonalMovement
+import chess.movementStrategy.validators.DistanceLimitMovement
+
+object NormalPieceMovement: MovementStrategyFactory {
+    private val strategy by lazy {
+        AndUnionMovementValidator(listOf(
+            DistanceLimitMovement(1), DiagonalMovement, EmptyDestination, NoPieceCanEat
+        ))
+    }
+    override fun getMovementStrategy(): MovementValidator {
+        return strategy
+    }
+}
