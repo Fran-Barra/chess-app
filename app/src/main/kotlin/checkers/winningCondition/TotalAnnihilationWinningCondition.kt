8082e5ae957b47e4950f9d1c93ccb00a36e3659b
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 15:15:32 -0300

    feat: TotalAnnihilationWinningCondition checkers

---
+++ app/src/main/kotlin/checkers/winningCondition/TotalAnnihilationWinningCondition.kt
@@ -0,0 +1,21 @@
+package checkers.winningCondition
+
+import Outcome
+import SuccessfulOutcome
+import boardGame.board.Vector
+import boardGame.game.Game
+import boardGame.piece.Piece
+import boardGame.player.Player
+import boardGame.winningConditionStrategy.WinningConditionStrategy
+
+class TotalAnnihilationWinningCondition: WinningConditionStrategy {
+    override fun checkWinningConditions(game: Game): Outcome<Boolean> {
+        return SuccessfulOutcome(getEnemyPieces(game.getBoard().getPiecesAndPosition(), game.getActualPlayer()).isEmpty())
+    }
+
+    private fun getEnemyPieces(pieces: List<Pair<Piece, Vector>>, actualPlayer: Player): List<Piece> {
+        return pieces.filter { (p, _) -> !actualPlayer.playerControlColor(p.getPieceColor()) }.map {
+                (p, _) -> p
+        }
+    }
+}
