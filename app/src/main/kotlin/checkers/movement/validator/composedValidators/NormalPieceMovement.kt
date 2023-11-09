e2ab3c62d5ac768949de229ba5f78418a0edd055
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 10:01:59 -0300

    fix: NormalPieceMovement

--- app/src/main/kotlin/checkers/movement/validator/composedValidators/NormalPieceMovement.kt
+++ app/src/main/kotlin/checkers/movement/validator/composedValidators/NormalPieceMovement.kt
@@ -5,13 +5,14 @@ import boardGame.movement.MovementValidator
 import boardGame.movement.unionMovement.AndUnionMovementValidator
 import checkers.movement.validator.EmptyDestination
 import checkers.movement.validator.NoPieceCanEat
+import checkers.movement.validator.ValidateAdvancing
 import chess.movementStrategy.validators.DiagonalMovement
 import chess.movementStrategy.validators.DistanceLimitMovement
 
 object NormalPieceMovement: MovementStrategyFactory {
     private val strategy by lazy {
         AndUnionMovementValidator(listOf(
-            DistanceLimitMovement(1), DiagonalMovement, EmptyDestination, NoPieceCanEat
+            DistanceLimitMovement(1), DiagonalMovement, EmptyDestination, ValidateAdvancing, NoPieceCanEat
         ))
     }
     override fun getMovementStrategy(): MovementValidator {
