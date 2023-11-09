870cbd492384b479133ceab285fee39d3f1f18f8
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 12:18:38 -0300

    feat: CheckerMovementFctory

---
+++ app/src/main/kotlin/checkers/movement/gameMovementsFactory/CheckersMovementsFactory.kt
@@ -0,0 +1,29 @@
+package checkers.movement.gameMovementsFactory
+
+import boardGame.movement.*
+import checkers.movement.performer.JumpAndEatPerformer
+import checkers.movement.validator.composedValidators.CheckerMovement
+import checkers.movement.validator.composedValidators.EatCheckerMovement
+import checkers.movement.validator.composedValidators.EatNormalMovement
+import checkers.movement.validator.composedValidators.NormalPieceMovement
+
+object CheckersMovementsFactory: GameMovementsFactory {
+    override fun getMovementsManager(): MovementManager {
+        val movements: Map<Int, List<Movement>> = mutableMapOf(
+            Pair(0, listOf(
+                Movement(CheckerMovement.getMovementStrategy(), FromTooMovementPerformer),
+                Movement(EatCheckerMovement.getMovementStrategy(), JumpAndEatPerformer)
+            )),
+            Pair(1, listOf(
+                Movement(NormalPieceMovement.getMovementStrategy(), FromTooMovementPerformer),
+                Movement(EatNormalMovement.getMovementStrategy(), JumpAndEatPerformer)
+            ))
+        )
+
+        return PieceTypeMovementManager(movements)
+    }
+
+    override fun getMovementsManagerController(): MovementManagerController {
+        return BaseMovementManagerController(listOf())
+    }
+}
