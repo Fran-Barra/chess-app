77f4c34325c2af61c91e99a1ed15f7bbfc192999
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 08:22:02 -0300

    feat: DistanceLimit checkers

---
+++ app/src/main/kotlin/checkers/movement/validator/DiagonalMovement.kt
@@ -0,0 +1,17 @@
+package checkers.movement.validator
+
+import boardGame.board.Board
+import boardGame.board.Vector
+import boardGame.movement.MovementValidator
+import boardGame.pieceEatingRuler.PieceEatingRuler
+import boardGame.player.Player
+import kotlin.math.abs
+
+object DiagonalMovement: MovementValidator {
+    override fun validate(pieceEatingRuler: PieceEatingRuler, player: Player, actual: Vector, destination: Vector,
+        board: Board
+    ): Boolean {
+        if (!board.positionExists(destination) || !board.positionExists(actual)) return false
+        return abs(actual.x - destination.x) != abs(actual.y - destination.y)
+    }
+}
