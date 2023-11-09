44cd9eeef6f005d8873811417740d126735da1f4
Franco B <franco.barravecchia2002@gmail.com>
2023-11-09 08:22:25 -0300

    feat: diagonalMovement checkers

---
+++ app/src/main/kotlin/checkers/movement/validator/DistanceLimitMovement.kt
@@ -0,0 +1,17 @@
+package checkers.movement.validator
+
+import boardGame.board.Board
+import boardGame.board.Vector
+import boardGame.distance
+import boardGame.movement.MovementValidator
+import boardGame.pieceEatingRuler.PieceEatingRuler
+import boardGame.player.Player
+
+class DistanceLimitMovement(val maxDistance: Int): MovementValidator {
+    override fun validate(pieceEatingRuler: PieceEatingRuler, player: Player, actual: Vector, destination: Vector,
+        board: Board
+    ): Boolean {
+        if (!board.positionExists(destination) || !board.positionExists(actual)) return false
+        return distance(actual, destination).toInt() <= maxDistance
+    }
+}
